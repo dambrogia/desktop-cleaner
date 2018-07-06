fea9cdc2ed5e7675eef8cb8ead1b548bd149eaa0
dominic dambrogia <domdambrogia@gmail.com>
2018-07-05 18:17:00 -0700

    first commit

---
+++ desktop-cleaner.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# #
+# Package: Desktop Cleaner
+# Author: Dominic Dambrogia
+# Version: 1.0.0
+# Website: dambrogia.com
+# Email: domdambrogia+desktopcleaner@gmail.com
+# #
+
+Y=`date '+%Y'`
+m=`date '+%m'`
+d=`date '+%d'`
+
+# Directory vars
+clean="$HOME/Desktop"
+save="$HOME/Desktop/save"
+storage="$HOME/Desktop/storage"
+current="$storage/$Y/$m/$d"
+symlink="$HOME/Desktop/current"
+
+# Create current folder if doesn't exist
+mkdir -p ${current}
+
+# Loop through and move all random items in desktop to our current dir
+for item in ${clean}/*; do
+    if [ "$item" != "$save" -a "$item" != "$storage" -a "$item" != "$symlink" ]; then
+        echo "mv $item $current"
+        mv $item $current
+    fi
+done
+
+# Remove symlink & reset
+[ -L $symlink ] && rm $symlink
+ln -s $current $symlink
+
+exit 0;
+
---
+++ readme.md
@@ -0,0 +1,18 @@
+## Desktop Cleaner
+_____
+Automagically clean your desktop with the help of the cron.
+
+#### Get started:
+
+    git clone https://github.com/dambrogia/desktop-cleaner
+
+#### Cron Setup:
+Add the following to your cron setup: (*nix)
+
+    $ crontab -e
+
+    * * * * * <full-path-to-repo>/desktop-cleaner.bash
+
+For help customizing your cron schedule visit: https://crontab.guru/
+
+Enjoy your clean desktop, dewd.
